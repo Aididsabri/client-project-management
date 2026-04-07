330c1d1c1950a2704db41d0cc75b325c400f749c
Aidid <aidid.sabri@kinesso.com>
2026-04-07 17:47:48 +0800

    add alert

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ let clientInput = document.querySelector(".clients__input")
 
 function addClient(){
     let name = clientInput.value
+    
+    if (!name){
+        alert("Client name cannot be empty")
+        return
+    }
+
     let listContainer = document.querySelector(".clients__list__container")
+    
     let newListBox = document.createElement("div")
     newListBox.classList.add("clients__list")
+    
     let newListName = document.createElement("div")
     newListName.classList.add("clients__list__name")
 
